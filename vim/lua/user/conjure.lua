fb59ffe675c5c7829249e03a8b37536723866741
Carl Hofmeister <hofmeister.carl@gmail.com>
2023-12-08 20:11:09 -0600

    Update some conjure settings

--- vim/lua/user/conjure.lua
+++ vim/lua/user/conjure.lua
@@ -1,6 +1,19 @@
 
 vim.g["conjure#filetype#fennel"] = "conjure.client.fennel.stdio"
-vim.g["conjure#client#fennel#stdio#command"] = "love ."
+-- If this is a love2d project, launch love 2d instead instead of regular repl
+local conf_file_path = vim.fn.expand('%:p:h') .. '/conf.lua'
+local is_love = false
+if vim.fn.filereadable(conf_file_path) == 1 then
+    for _, line in ipairs(vim.fn.readfile(conf_file_path)) do
+        if string.find(line, 'love') then
+            is_love = true
+            break
+        end
+    end
+end
+if is_love then
+    vim.g["conjure#client#fennel#stdio#command"] = "love ."
+end
 vim.g["conjure#filetypes"] = {"clojure", "fennel", "janet", "racket", "scheme", "reader"}
 
 vim.keymap.set("n", "<LocalLeader>eR", ":ConjureFnlEvalReload<cr>", {noremap = true, silent = true})
